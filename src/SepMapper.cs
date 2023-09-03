719b754c26f1c9a2bac4be6f029fc789071981db
Abgier Avraha <abgier.avraha@gmail.com>
2023-09-03 17:52:29 +1000

    wip

--- src/SepMapper.cs
+++ src/SepMapper.cs
@@ -1,11 +1,148 @@
 namespace SepMapper;
 
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Runtime.InteropServices;
 using nietras.SeparatedValues;
 
-public class SepMapper
-{
 
-    public static void Test()
+public interface ISepMapperContext
+{
+    public SepMapperRulesForType<T> RegisterClass<T>() where T : class;
+    public void UnregisterClass<T>() where T : class;
+    public string Serialize<T>(T anyObject) where T : class;
+    public List<T> Parse<T>(string csv) where T : class;
+    public List<T> ParseFile<T>(string path) where T : class;
+}
+
+public class SepMapperContext : ISepMapperContext
+{
+    private readonly Dictionary<Type, dynamic> mappers = new();
+
+    public SepMapperRulesForType<T> RegisterClass<T>() where T : class
     {
+        var typeMapper = new SepMapperRulesForType<T>();
+
+        this.mappers.Add(typeof(T), typeMapper);
+        return typeMapper;
     }
+
+    public void UnregisterClass<T>() where T : class
+    {
+        this.mappers.Remove(typeof(T));
+    }
+
+    public string Serialize<T>(T anyObject) where T : class
+    {
+        // TODO: use mapper else default behaviour
+        return "";
+    }
+
+    public List<T> Parse<T>(string csv) where T : class
+    {
+        using var reader = Sep.Reader().FromText(csv);
+
+        return this.Parse<T>(reader);
+    }
+
+    public List<T> ParseFile<T>(string path) where T : class
+    {
+        using var reader = Sep.Reader().FromFile(path);
+
+        return this.Parse<T>(reader);
+    }
+
+    private List<T> Parse<T>(SepReader reader) where T : class
+    {
+        var model = (T)Activator.CreateInstance(typeof(T), new object[] { })!;
+
+        if (!this.mappers.ContainsKey(typeof(T)))
+        {
+            throw new Exception($"No mapping class registered for type: {typeof(T)}");
+        }
+
+        var mapper = (SepMapperRulesForType<T>)this.mappers[typeof(T)];
+
+        var list = new List<T>();
+
+        foreach (var readRow in reader)
+        {
+            foreach (var col in reader.Header.ColNames)
+            {
+                // TODO: dynamic generic param
+                var rule = mapper.GetRule<string>(col);
+                if (rule?.Accessor is not null)
+                {
+                    var setter = GetSetter(rule.Accessor);
+
+                    if (rule.ToProperty != null)
+                    {
+                        // Parse with ToProperty transformation
+                        setter.Invoke(model, rule.ToProperty(readRow[col].ToString()));
+                        list.Add(model);
+                    }
+                    else
+                    {
+                        // Default parse
+                        setter.Invoke(model, readRow[col].ToString());
+                        list.Add(model);
+                    }
+                }
+            }
+        }
+
+        return list;
+    }
+
+    private static Action<T, TProperty> GetSetter<T, TProperty>(Expression<Func<T, TProperty>> expression)
+    {
+        var memberExpression = (MemberExpression)expression.Body;
+        var property = (PropertyInfo)memberExpression.Member;
+        var setMethod = property.GetSetMethod();
+
+        var parameterT = Expression.Parameter(typeof(T), "x");
+        var parameterTProperty = Expression.Parameter(typeof(TProperty), "y");
+
+        var newExpression =
+            Expression.Lambda<Action<T, TProperty>>(
+                Expression.Call(parameterT, setMethod!, parameterTProperty),
+                parameterT,
+                parameterTProperty
+            );
+
+        return newExpression.Compile();
+    }
+}
+
+public class SepMapperRulesForType<T> where T : class
+{
+    private readonly Dictionary<string, dynamic> rules = new();
+
+    public SepMapperRulesForType<T> AddRule<P>(string key, Expression<Func<T, P>> accessor, [Optional] Func<string, P> toProperty, [Optional] Func<P, string> toCsv) where P : class
+    {
+        var propertyRules = new SepMapperTypeRulesForProperty<T, P>(accessor)
+        {
+            ToProperty = toProperty,
+            ToCsv = toCsv,
+        };
+        this.rules.Add(key, propertyRules);
+        return this;
+    }
+
+    public SepMapperTypeRulesForProperty<T, P>? GetRule<P>(string key) where P : class
+    {
+        return (SepMapperTypeRulesForProperty<T, P>)this.rules[key];
+    }
+}
+
+public class SepMapperTypeRulesForProperty<T, P> where T : class where P : class
+{
+    public SepMapperTypeRulesForProperty(Expression<Func<T, P>> Accessor)
+    {
+        this.Accessor = Accessor;
+    }
+
+    public Expression<Func<T, P>> Accessor;
+    public Func<string, P>? ToProperty;
+    public Func<P, string>? ToCsv;
 }
--- tests/TestSepMapper.cs
+++ tests/TestSepMapper.cs
@@ -1,5 +1,6 @@
 namespace SepMapper;
 
+using System.Linq.Expressions;
 using System.Reflection;
 using System.Runtime.InteropServices;
 using nietras.SeparatedValues;
@@ -8,109 +9,10 @@ public class TestSepMapper
 {
     public class Something
     {
-        public string SomeString { get; set; } = "";
+        public string SomeStringA { get; set; } = "";
+        public string SomeStringB { get; set; } = "";
         public int SomeInt { get; set; }
         public DateTimeOffset SomeDate { get; set; }
-    }
-
-    public interface ISepMapperContext
-    {
-        public SepMapperRulesForType<T> RegisterClass<T>() where T : class;
-        public void UnregisterClass<T>() where T : class;
-        public string Serialize<T>(T anyObject) where T : class;
-        public T Parse<T>(string csv) where T : class;
-        public T ParseFile<T>(string path) where T : class;
-    }
-
-    public class SepMapperContext : ISepMapperContext
-    {
-        private readonly Dictionary<Type, SepMapperRulesForType<object>> mappers = new();
-
-        public SepMapperRulesForType<T> RegisterClass<T>() where T : class
-        {
-            var typeMapper = new SepMapperRulesForType<T>();
-
-            // TODO: Safe cast and Convert.ChangeType?
-            this.mappers.Add(typeof(T), (SepMapperRulesForType<object>)typeMapper);
-            return typeMapper;
-        }
-
-        public void UnregisterClass<T>() where T : class
-        {
-            this.mappers.Remove(typeof(T));
-        }
-
-        public string Serialize<T>(T anyObject) where T : class
-        {
-            // TODO: use mapper else default behaviour
-            return "";
-        }
-
-        public T Parse<T>(string csv) where T : class
-        {
-            using var reader = Sep.Reader().FromText(csv);
-            var model = (T)Activator.CreateInstance(typeof(T), new object[] { })!;
-
-            if (!this.mappers.ContainsKey(typeof(T)))
-            {
-                return model;
-            }
-
-            var mapper = this.mappers[typeof(T)];
-
-            foreach (var col in reader.Header.ColNames)
-            {
-                var rule = mapper.GetRule<object>(col);
-                if (rule?.Accessor is not null)
-                {
-                    var property = rule.Accessor(model);
-                    property = "test";
-                }
-            }
-
-            return model;
-        }
-
-        public T ParseFile<T>(string path) where T : class
-        {
-            // TODO: use mapper else default behaviour
-            var model = (T)Activator.CreateInstance(typeof(T), new object[] { })!;
-            return model;
-        }
-    }
-
-    public class SepMapperRulesForType<T> where T : class
-    {
-        private readonly Dictionary<string, SepMapperTypeRulesForProperty<object, object>> rules = new();
-
-        public SepMapperRulesForType<T> AddRule<P>(string key, Func<T, P> accessor, [Optional] Func<string, P> toProperty, [Optional] Func<P, string> toCsv) where P : class
-        {
-            var propertyRules = new SepMapperTypeRulesForProperty<T, P>(accessor)
-            {
-                ToProperty = toProperty,
-                ToCsv = toCsv,
-            };
-            // TODO: Safe cast and Convert.ChangeType
-            this.rules.Add(key, (SepMapperTypeRulesForProperty<object, object>)propertyRules);
-            return this;
-        }
-
-        public SepMapperTypeRulesForProperty<T, P>? GetRule<P>(string key) where P : class
-        {
-            return this.rules[key] as SepMapperTypeRulesForProperty<T, P>;
-        }
-    }
-
-    public class SepMapperTypeRulesForProperty<T, P> where T : class where P : class
-    {
-        public SepMapperTypeRulesForProperty(Func<T, P> Accessor)
-        {
-            this.Accessor = Accessor;
-        }
-
-        public Func<T, P> Accessor;
-        public Func<string, P>? ToProperty;
-        public Func<P, string>? ToCsv;
     }
 
     [Fact]
@@ -120,29 +22,27 @@ public class TestSepMapper
 
         registry
             .RegisterClass<Something>()
-            .AddRule("SomeString",
-                accessor: a => a.SomeString
-                // toProperty: a => a,
+            .AddRule("SomeStringA",
+                accessor: a => a.SomeStringA
+            )
+            .AddRule("SomeStringB",
+                accessor: a => a.SomeStringB,
+                toProperty: a => a.ToUpper()
                 // toCsv: a => a
             );
 
 
         var text = """
-        SomeString
-        wowee
+        SomeStringA,SomeStringB
+        raw,transformed
         """;
 
         var result = registry.Parse<Something>(text);
 
-        Assert.Equal(new Something()
+        Assert.Equivalent(new Something()
         {
-            SomeString = "test"
-        }, result);
-
-        // using var reader = Sep.Reader().FromText(text);
-        // using var writer = reader.Spec.Writer().ToText();
-
-        // var idx = reader.Header.IndexOf("B");
-        // var nms = new[] { "E", "F" };
+            SomeStringA = "raw",
+            SomeStringB = "TRANSFORMED",
+        }, result[0]);
     }
 }
